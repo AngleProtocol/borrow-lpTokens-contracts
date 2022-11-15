aa55ceea319a709ed208ad444314ade5d68d7869
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2022-11-15 16:57:49 +0100

    removed import ethereum-waffle

--- test/hardhat/utils/chai-setup.ts
+++ test/hardhat/utils/chai-setup.ts
@@ -1,8 +1,6 @@
 import chaiModule from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { solidity } from 'ethereum-waffle';
 
 chaiModule.use(chaiAsPromised);
-chaiModule.use(solidity);
 
 export = chaiModule;
