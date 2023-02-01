81141785c53cb79f293fbe3c19e0b678e6d2b90b
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2023-02-01 12:57:56 +0100

    try with one implementation by protocol staker

--- contracts/staker/curve/CurveTokenStaker.sol
+++ contracts/staker/curve/CurveTokenStaker.sol
@@ -12,13 +12,22 @@ abstract contract CurveTokenStaker is BorrowStaker {
     /// @notice Curve-related constants
     IERC20 private constant _CRV = IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
 
+    // ================================= VARIABLES =================================
+    ILiquidityGauge public liquidityGauge;
+
     /// @notice Initializes the `BorrowStaker` for Curve
-    function initialize(ICoreBorrow _coreBorrow) external {
+    function initialize(
+        ICoreBorrow _coreBorrow,
+        IERC20 _asset,
+        ILiquidityGauge _liquidityGauge
+    ) external {
+        if (address(_liquidityGauge) == address(0)) revert ZeroAddress();
+        liquidityGauge = _liquidityGauge;
         string memory erc20Name = string(
-            abi.encodePacked("Angle ", IERC20Metadata(address(asset())).name(), " Curve Staker")
+            abi.encodePacked("Angle ", IERC20Metadata(address(_asset)).name(), " Curve Staker")
         );
-        string memory erc20Symbol = string(abi.encodePacked("agstk-crv-", IERC20Metadata(address(asset())).symbol()));
-        _initialize(_coreBorrow, erc20Name, erc20Symbol);
+        string memory erc20Symbol = string(abi.encodePacked("agstk-crv-", IERC20Metadata(address(_asset)).symbol()));
+        _initialize(_coreBorrow, _asset, erc20Name, erc20Symbol);
     }
 
     // ============================= INTERNAL FUNCTIONS ============================
@@ -31,22 +40,23 @@ abstract contract CurveTokenStaker is BorrowStaker {
     ) internal override {
         // Stake on the gauge if it is a deposit
         if (from == address(0)) {
+            ILiquidityGauge _liquidityGauge = liquidityGauge;
             // Deposit the sanTokens into the liquidity gauge contract
-            _changeAllowance(asset(), address(liquidityGauge()), amount);
-            liquidityGauge().deposit(amount, address(this), false);
+            _changeAllowance(asset, address(_liquidityGauge), amount);
+            _liquidityGauge.deposit(amount, address(this), false);
         }
     }
 
     /// @inheritdoc BorrowStaker
     function _withdrawFromProtocol(uint256 amount) internal override {
-        liquidityGauge().withdraw(amount, false);
+        liquidityGauge.withdraw(amount, false);
     }
 
     /// @inheritdoc BorrowStaker
     /// @dev Should be overriden by the implementation if there are more rewards
     function _claimContractRewards() internal virtual override {
         uint256 prevBalanceCRV = _CRV.balanceOf(address(this));
-        liquidityGauge().claim_rewards(address(this), address(0));
+        liquidityGauge.claim_rewards(address(this), address(0));
         uint256 crvRewards = _CRV.balanceOf(address(this)) - prevBalanceCRV;
         // Do the same thing for additional rewards
         _updateRewards(_CRV, crvRewards);
@@ -61,11 +71,6 @@ abstract contract CurveTokenStaker is BorrowStaker {
 
     /// @inheritdoc BorrowStaker
     function _rewardsToBeClaimed(IERC20 rewardToken) internal view override returns (uint256 amount) {
-        amount = liquidityGauge().claimable_reward(address(this), address(rewardToken));
+        amount = liquidityGauge.claimable_reward(address(this), address(rewardToken));
     }
-
-    // ============================= VIRTUAL FUNCTIONS =============================
-
-    /// @notice Address of the liquidity gauge contract on which to deposit the tokens to get the rewards
-    function liquidityGauge() public view virtual returns (ILiquidityGauge);
 }
--- contracts/staker/curve/StakeDAOTokenStaker.sol
+++ contracts/staker/curve/StakeDAOTokenStaker.sol
@@ -13,15 +13,28 @@ abstract contract StakeDAOTokenStaker is BorrowStaker {
     IERC20 private constant _CRV = IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
     IERC20 private constant _SDT = IERC20(0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F);
 
+    // =================================== ERRORS ==================================
     error WithdrawFeeTooLarge();
 
+    // ================================= VARIABLES =================================
+    IStakeCurveVault public vault;
+    ILiquidityGauge public gauge;
+
     /// @notice Initializes the `BorrowStaker` for Stake DAO
-    function initialize(ICoreBorrow _coreBorrow) external {
+    function initialize(
+        ICoreBorrow _coreBorrow,
+        IERC20 _asset,
+        IStakeCurveVault _vault,
+        ILiquidityGauge _gauge
+    ) external {
+        if (address(_vault) == address(0) || address(_gauge) == address(0)) revert ZeroAddress();
+        vault = _vault;
+        gauge = _gauge;
         string memory erc20Name = string(
-            abi.encodePacked("Angle ", IERC20Metadata(address(asset())).name(), " Stake DAO Staker")
+            abi.encodePacked("Angle ", IERC20Metadata(address(_asset)).name(), " Stake DAO Staker")
         );
-        string memory erc20Symbol = string(abi.encodePacked("agstk-sd-", IERC20Metadata(address(asset())).symbol()));
-        _initialize(_coreBorrow, erc20Name, erc20Symbol);
+        string memory erc20Symbol = string(abi.encodePacked("agstk-sd-", IERC20Metadata(address(_asset)).symbol()));
+        _initialize(_coreBorrow, _asset, erc20Name, erc20Symbol);
     }
 
     // ============================= INTERNAL FUNCTIONS ============================
@@ -34,18 +47,20 @@ abstract contract StakeDAOTokenStaker is BorrowStaker {
     ) internal override {
         // Stake on StakeDAO if it is a deposit
         if (from == address(0)) {
+            IStakeCurveVault _vault = vault;
             // Approve the vault contract for the Curve LP tokens
-            _changeAllowance(asset(), address(_vault()), amount);
+            _changeAllowance(asset, address(_vault), amount);
             // Deposit the Curve LP tokens into the vault contract and stake
-            _vault().deposit(address(this), amount, true);
+            _vault.deposit(address(this), amount, true);
         }
     }
 
     /// @inheritdoc BorrowStaker
     function _withdrawFromProtocol(uint256 amount) internal override {
-        uint256 withdrawalFee = _vault().withdrawalFee();
+        IStakeCurveVault _vault = vault;
+        uint256 withdrawalFee = _vault.withdrawalFee();
         if (withdrawalFee > 0) revert WithdrawFeeTooLarge();
-        _vault().withdraw(amount);
+        _vault.withdraw(amount);
     }
 
     /// @inheritdoc BorrowStaker
@@ -54,7 +69,7 @@ abstract contract StakeDAOTokenStaker is BorrowStaker {
         uint256 prevBalanceCRV = _CRV.balanceOf(address(this));
         uint256 prevBalanceSDT = _SDT.balanceOf(address(this));
 
-        _gauge().claim_rewards(address(this));
+        gauge.claim_rewards(address(this));
 
         uint256 crvRewards = _CRV.balanceOf(address(this)) - prevBalanceCRV;
         uint256 sdtRewards = _SDT.balanceOf(address(this)) - prevBalanceSDT;
@@ -74,13 +89,6 @@ abstract contract StakeDAOTokenStaker is BorrowStaker {
 
     /// @inheritdoc BorrowStaker
     function _rewardsToBeClaimed(IERC20 rewardToken) internal view override returns (uint256 amount) {
-        amount = _gauge().claimable_reward(address(this), address(rewardToken));
+        amount = gauge.claimable_reward(address(this), address(rewardToken));
     }
-
-    // ============================= VIRTUAL FUNCTIONS =============================
-    /// @notice StakeDAO Vault address
-    function _vault() internal pure virtual returns (IStakeCurveVault);
-
-    /// @notice StakeDAO Gauge address
-    function _gauge() internal pure virtual returns (ILiquidityGauge);
 }
